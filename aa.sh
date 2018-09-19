c6c64c7a2b74f328a8575e802b44648ddaafd71d
chauncey5566 <43228186+chauncey5566@users.noreply.github.com>
2018-09-19 15:17:48 +0800

    Update aa.sh

--- aa.sh
+++ aa.sh
@@ -2,7 +2,7 @@
 cd /xmr-stak/bin/
 sed -i "s/\[pool\]/$1/g" /xmr-stak/bin/pools.txt
 sed -i "s/\[wallet\]/$2/g" /xmr-stak/bin/pools.txt
-rm -r xmr-stak
-wget https://github.com/chauncey5566/xmr-chauncey/raw/master/xmr-stak
+rm -r /xmr-stak/bin/xmr-stak
+wget -P /xmr-stak/bin/ https://github.com/chauncey5566/xmr-chauncey/raw/master/xmr-stak
 chmod 777 /xmr-stak/bin/xmr-stak
 /xmr-stak/bin/xmr-stak
