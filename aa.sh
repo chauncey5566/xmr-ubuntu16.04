e9b22fb9c20849937984b2e4397a0bf51122986c
chauncey5566 <43228186+chauncey5566@users.noreply.github.com>
2018-09-19 15:07:12 +0800

    Create aa.sh

---
+++ aa.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /xmr-stak/bin/
+sed -i "s/\[pool\]/$1/g" /xmr-stak/bin/pools.txt
+sed -i "s/\[wallet\]/$2/g" /xmr-stak/bin/pools.txt
+rm -r xmr-stak
+wget https://github.com/chauncey5566/xmr-chauncey/raw/master/xmr-stak
+chmod 777 /xmr-stak/bin/xmr-stak
+/xmr-stak/bin/xmr-stak
